f4b145c3bd3b494ff74ba6d1b2ea6d5af7e1eb11
lefterav <lefterav@pindi.me>
2017-07-12 18:07:46 +0300

    Mass chart per decimiles, Gauge color based on score level

--- qe/charts.py
+++ qe/charts.py
@@ -23,9 +23,19 @@ class AverageScoreChart(Chart):
     
     def get_datasets(self, average_score, max_score=1.0):
         
+        if average_score <= 0.25*max_score:
+            color = "DC143C" #red
+        elif average_score < 0.5*max_score:
+            color = "#FFA07A" #light salmon/orange
+        elif average_score < 0.75*max_score:
+            color = "#FFD700" #gold
+        elif average_score < 0.9*max_score: 
+            color = "#98FB98" #palegreen
+        else:
+            color = "#228B22" #forestgreen
  
         colors = [
-            "#FF6384",
+            color,
             "#eeeeee"
         ]
         
@@ -50,7 +60,7 @@ class ScoreMassChart(Chart):
                        scaleLabel=ScaleLabel(display=True, 
                                              labelString='number of sentences',
                                              ),
-                       ticks={'stepSize': 1},
+                       #ticks={'stepSize': 1},
                        )
                   ],
     }
@@ -60,19 +70,58 @@ class ScoreMassChart(Chart):
         translation_evaluations = \
             MachineTranslationEvaluation.objects. \
             filter(translation__source__document_id=document_id)
-        scores = sorted([t.score for t in translation_evaluations])
+        scores = sorted([round(t.score, 1) for t in translation_evaluations])
         
-        data = []
-        for score in scores:
+        q1=0
+        q2=0
+        q3=0
+        q4=0
+        
+        total = len(scores)
+        
+        dotsdata = []
+        linedata = []
+        for score in sorted(list(set(scores))):
             evaluations_per_score = \
                 MachineTranslationEvaluation.objects. \
                 filter(translation__source__document_id=document_id,
                        score=score).count()
                    
-            data.append({'x': score, 'y': evaluations_per_score})
-        return [DataSet(type='line',
-                        data=data, 
-                        label="mass per quality score"
+            dotsdata.append({'x': score, 'y': 100.0*evaluations_per_score/total})
+            
+            # split in quartiles:
+
+            
+        #for score in scores:
+        #    if score < 0.25:
+        #        q1+=1
+        #    elif score < 0.5:
+        #        q2+=1
+        #    elif score < 0.75:
+        #        q3+=1
+        #    else:
+        #        q4+=1
+        
+        #total = total*25
+        
+        #linedata.append({'x': 0, 'y': q1*100.0/total})
+        #linedata.append({'x': 0.5, 'y': q2*100.0/total})
+        #linedata.append({'x': 0.75, 'y': q3*100.0/total})
+        #linedata.append({'x': 1.00, 'y': q4*100.0/total})
+        #linedata.append({'x': 1.0, 'y': q4*100.0/total})
+
+            
+        return [
+                #DataSet(type='line',
+                #        data=linedata, 
+                #        label="quartile tension line",
+                #        pointRadius=1,
+                #        fill=False,
+                #        ),
+                DataSet(type='line',
+                        data=dotsdata, 
+                        label="mass per decimile",
+                        pointRadius=1,
                         ),
                 ]
         
