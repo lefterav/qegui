ed4deb113201e9057830b9a269e9da2afa60152e
lefterav <lefterav@pindi.me>
2017-08-25 22:13:55 +0300

    Renamed 'mtqual' frame project to 'qegui'

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mtqual.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qegui.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
